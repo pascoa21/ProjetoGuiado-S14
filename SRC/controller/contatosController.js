66c73a51c882929bbd6e8fc4ca77955d83d38a5d
Jessyca Maria Pascoa <50502920+pascoa21@users.noreply.github.com>
2020-11-13 22:53:29 -0300

    Predefinição das rotas

--- SRC/controller/contatosController.js
+++ SRC/controller/contatosController.js
@@ -5,3 +5,23 @@ exports.GetAll = (req,res,next)=>{
 exports.Add = (req,res,next)=>{
     res.status(201).send(req.body)
 }
+
+exports.getName = (req,res,next)=>{
+    res.status(201).send(req.body)
+}
+
+exports.getById = (req,res,next)=>{
+    res.status(201).send(req.body)
+}
+
+exports.deleteContact = (req,res,next)=>{
+    res.status(201).send(req.body)
+}
+
+exports.UpNumberPhone = (req,res,next)=>{
+    res.status(201).send(req.body)
+}
+
+exports.upContato = (req,res,next)=>{
+    res.status(201).send(req.body)
+}
--- SRC/routes/contatoRoute.js
+++ SRC/routes/contatoRoute.js
@@ -2,9 +2,19 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controller/contatosController")
 
+//retorna todos os contatos
 router.get('/contatos/',controller.getAll)
-
+//criar um novo contato
 router.post('/contatos/criar',controller.Add)
-
+//busca contato e retorna por nome
 router.get('/contatos/:nome/',controller.getName)
+//busca contato por ID
+router.get('/contatos/:id/',controller.getById)
+//exclui contato
+router.delete('/deletar/:id/',controller.deleteContact)
+//modifica o numero do celular
+router.put('/atualizar/telefone/:id', controller.UpNumberPhone)
+//modifica todo cadastro
+router.put('/atualizar/:id',controller.upContato)
+
 module.exports = router
